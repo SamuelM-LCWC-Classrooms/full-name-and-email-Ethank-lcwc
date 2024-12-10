eec446c5a66620d609f0ca550bd3bc52a2dbd3d2
Ethank-lcwc <144791953+Ethank-lcwc@users.noreply.github.com>
2024-12-10 14:07:07 +0000

    helllloooooooooo

--- employee.py
+++ employee.py
@@ -1,7 +1,5 @@
 class Employee:
     def __init__(self,fname:str,sname:str):
-        self.__fname = fname
-        self.__sname = sname
-        fullname = self.__fname + " " + self.__sname
-        email = self.__fname + "." + self.__sname + "@company.com"
-        firstname = self.__fname
+        fullname = fname + " " + sname
+        email = fname + "." + sname + "@company.com"
+        firstname = fname
